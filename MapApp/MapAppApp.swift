2b61a4f69857b9e8fae060a9cd5a61b4178e627a
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-08 16:07:59 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Jiten Patel on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
