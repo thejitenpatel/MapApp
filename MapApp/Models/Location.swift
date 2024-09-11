39c92eef6d02b880946f2ab06839e9c35e5e5f67
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-11 07:30:17 +0530

    Added default location on map

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -8,7 +8,7 @@
 import Foundation
 import MapKit
 
-struct Location {
+struct Location: Identifiable {
     
     let name: String
     let cityName: String
@@ -16,6 +16,10 @@ struct Location {
     let description: String
     let imageNames: [String]
     let link: String
+    
+    var id: String {
+        name + cityName
+    }
     
 }
 
--- MapApp/ViewModels/LocationViewModel.swift
+++ MapApp/ViewModels/LocationViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    
+    @Published var locations: [Location]
+    
+    @Published var mapLocation: Location {
+        didSet {
+            upateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first! // Do not try
+        self.upateMapRegion(location: locations.first!)
+    }
+    
+    private func upateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
+    }
+    
+}
