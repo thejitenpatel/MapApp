9a7d51ee5f098fa80953eb59273d1bab26e023d9
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-11 07:29:16 +0530

    Added Default location on the map

---
+++ MapApp/ViewModels/LocationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationViewModel.swift
+//  MapApp
+//
+//  Created by Jiten Patel on 11/09/24.
+//
+
+import Foundation
---
+++ MapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  MapApp
+//
+//  Created by Jiten Patel on 11/09/24.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
